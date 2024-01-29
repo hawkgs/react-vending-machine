36c78a485683c7d2ea73981ab5254c0ace90a83f
hawkgs <hawk.dvp@gmail.com>
2024-01-29 20:37:48 +0200

    Integrate state management with the UI

--- src/components/machine/Machine.tsx
+++ src/components/machine/Machine.tsx
@@ -1,27 +1,49 @@
 import { useState } from 'react';
-import { Machine as MachineState } from '../../state/models';
+
 import Screen from '../screen/Screen';
 import Inventory from '../inventory/Inventory';
 import Controls from '../controls/Controls';
+import CoinsSlot from '../coins-slot/CoinsSlot';
+
+import { Coin, Machine as MachineState } from '../../state/models';
+import { update } from '../../state/update';
+import {
+  dispenseChangeAttempt,
+  enterCode,
+  insertCoin,
+} from '../../state/actions';
 
 export default function Machine() {
   const [machineState, setMachineState] = useState(new MachineState({}));
 
-  const onCodeEnter = (code: number) => {};
+  const onCodeEnter = (code: number) => {
+    const machine = update(machineState, enterCode(code));
+    setMachineState(machine);
+  };
 
-  const onCoinsDispense = () => {};
+  const onCoinsDispense = () => {
+    const machine = update(
+      machineState,
+      dispenseChangeAttempt(machineState.coinsInSlot),
+    );
+    setMachineState(machine);
+  };
 
-  const onCoinInserted = (coin: number) => {};
+  const onCoinInserted = (coin: Coin) => {
+    const machine = update(machineState, insertCoin(coin));
+    setMachineState(machine);
+  };
 
   return (
     <div className="machine">
-      <Screen machine={machineState} />
       <Inventory machine={machineState} />
+      <Screen machine={machineState} />
       <Controls
         machine={machineState}
         onCodeEnter={onCodeEnter}
         onCoinsDispense={onCoinsDispense}
       />
+      <CoinsSlot machine={machineState} onCoinInserted={onCoinInserted} />
     </div>
   );
 }
