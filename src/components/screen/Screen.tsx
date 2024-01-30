c08bd700f687d1501b43b4824fc165dab9dfe837
hawkgs <hawk.dvp@gmail.com>
2024-01-30 20:29:50 +0200

    Implement the initial version of the Screen

--- src/components/screen/Screen.tsx
+++ src/components/screen/Screen.tsx
@@ -1,10 +1,47 @@
-import './Screen.module.css';
-import { Machine } from '../../state/models';
+import { useEffect, useRef, useState } from 'react';
+import styles from './Screen.module.css';
+
+import { List } from 'immutable';
+import { Machine, State } from '../../state/models';
 
 interface ScreenProps {
   machine: Machine;
 }
 
+const Messages: { [key: string]: string } = {
+  [State.StandBy]: 'Machine is ready for use!',
+  [State.ItemNotFound]: 'Item not found.',
+  [State.InsufficientCredit]: 'Insufficient credit',
+  [State.ItemUnavailable]: 'The item is unavailable',
+  [State.CantProcessOrder]: `Can't process order`,
+};
+
 export default function Screen({ machine }: ScreenProps) {
-  return <div></div>;
+  const [messageHistory, setMessageHistory] = useState<List<string>>(List([]));
+  const ulList = useRef<HTMLUListElement | null>(null);
+
+  useEffect(() => {
+    let message = '';
+
+    if (machine.state === State.CoinsInserted) {
+      const credit = machine.coinsInSlot.reduce((p, n) => p + n, 0) / 100;
+      message = `Inserted a coin. Current credit: $${credit}`;
+    } else {
+      message = Messages[machine.state];
+    }
+
+    setMessageHistory((messageHistory) => messageHistory.push(message));
+  }, [machine]);
+
+  useEffect(() => {
+    ulList?.current?.lastElementChild?.scrollIntoView();
+  }, [messageHistory]);
+
+  return (
+    <ul ref={ulList} className={styles.screen}>
+      {messageHistory.map((msg, idx) => (
+        <li key={msg + idx}>{msg}</li>
+      ))}
+    </ul>
+  );
 }
