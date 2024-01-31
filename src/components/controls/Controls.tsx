17b3d13c75b1af64ca86583d6b367a2e0d816d93
hawkgs <hawk.dvp@gmail.com>
2024-02-01 00:15:07 +0200

    Implement the init version of the Controls

--- src/components/controls/Controls.tsx
+++ src/components/controls/Controls.tsx
@@ -10,8 +10,33 @@ export default function Controls({
   onCodeEnter,
   onCoinsDispense,
 }: ControlsProps) {
-  // const [code, setCode] = useState<string>('');
-  // const onButtonClick = (digit: string) => {};
+  const [code, setCode] = useState<string>('');
 
-  return <div></div>;
+  const onButtonClick = (digit: string) => {
+    if (code.length > 1) {
+      setCode(digit);
+    } else {
+      const newCode = code + digit;
+      setCode(newCode);
+
+      if (newCode.length > 1) {
+        onCodeEnter(newCode);
+      }
+    }
+  };
+
+  return (
+    <>
+      <div>CODE: {code}</div>
+      <button onClick={onCoinsDispense}>Dispense</button>
+      <div>
+        <button onClick={() => onButtonClick('1')}>1</button>
+        <button onClick={() => onButtonClick('2')}>2</button>
+        <button onClick={() => onButtonClick('3')}>3</button>
+        <button onClick={() => onButtonClick('4')}>4</button>
+        <button onClick={() => onButtonClick('5')}>5</button>
+        <button onClick={() => onButtonClick('6')}>6</button>
+      </div>
+    </>
+  );
 }
